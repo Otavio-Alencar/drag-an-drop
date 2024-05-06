29ed6f04d83816f13ac12f441b959f0a7cd28b55
Otávio Alencar <oalencardev@gmail.com>
2024-05-06 20:36:01 -0300

    drag and drop area

---
+++ script.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('dragstart',dragStart)
+    item.addEventListener('dragend',dragEnd)
+})
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover',dragOver)
+    area.addEventListener('dragleave',dragLeave)
+    area.addEventListener('drop',drop)
+})
+function dragStart(e){
+    e.currentTarget.classList.add('dragging')
+}
+
+function dragEnd(e){
+    e.currentTarget.classList.remove('dragging')
+}
+
+function dragOver(e){
+    if(e.currentTarget.querySelector('.item') === null){
+        e.preventDefault()
+        e.currentTarget.classList.add('hover')
+    }
+    
+    
+}
+
+function dragLeave(e){
+    e.currentTarget.classList.remove('hover')
+}
+function drop(e){
+    e.currentTarget.classList.remove('hover')
+    
+    
+    if(e.currentTarget.querySelector('.item') === null){
+        let dragItem = document.querySelector('.item.dragging')
+        e.currentTarget.appendChild(dragItem)
+
+    }
+}
